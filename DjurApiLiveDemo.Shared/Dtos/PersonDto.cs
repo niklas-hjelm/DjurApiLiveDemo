9717a4d38ce1ecede881fe82e99fb80006a699bf
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-29 09:58:10 +0100

    Added Blazor-project and MudBlazor

---
+++ DjurApiLiveDemo.Shared/Dtos/PersonDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DjurApiLiveDemo.Shared.Dtos;
+
+public class PersonDto
+{
+    [Required, StringLength(15, MinimumLength = 2)]
+    public string Name { get; set; }
+    [Required, Phone]
+    public string Phone { get; set; }
+}
